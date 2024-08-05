b8006d96988fbec9182ec980e6a50ffdc41c4ed2
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2024-08-05 09:22:06 +0200

    columns

--- template-parts/content/archive-zimowy.php
+++ template-parts/content/archive-zimowy.php
@@ -22,25 +22,33 @@ do_action('kadence_hero_header');
                     $wyjazd_zimowy = new pojedynczy_zimowy(get_field('tytul'), get_field('termin'), get_field('opis'), get_field('opis_extra'), get_field('zdj')) ?>
                     <!-- WPIS -->
                     <div class="container">
-                        <h1><?php echo $wyjazd_zimowy->title ?></h1>
-                        <img className='zdjecieWyrozniajace' alt={product.acf.zdj.id} src=<?php echo $wyjazd_zimowy->photo ?> width="500" height="300" />
-                        <p><?php echo $wyjazd_zimowy->date ?></p>
-                        <p><?php echo $wyjazd_zimowy->description ?></p>
-                        <!-- <td> <a href="<?php the_permalink(); ?>"> Zobacz</a></td> -->
-                        <a type="button" class="btn btn-outline-secondary" href="<?php the_permalink(); ?>">Zobacz więcej</a>
-                        <!-- <p><?php echo $wyjazd_zimowy->description_extra ?></p> -->
-                    </div>
-                    <!-- KONIEC WPISU -->
-                <?php endwhile; ?>
-            <?php endif; ?>
-            <?php {
-                get_template_part('template-parts/content/error');
-            }
-            /**
-             * Hook for anything after main content
-             */
-            do_action('kadence_after_main_content');
-            ?>
+                        <div class="row">
+                            <h1><?php echo $wyjazd_zimowy->title ?></h1>
+                            <p><?php echo $wyjazd_zimowy->date ?></p>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <img className='zdjecieWyrozniajace' alt='zdjecie wyrozniajace' src=<?php echo $wyjazd_zimowy->photo ?> width="300" height="150" />
+                            </div>
+                            <div class="col">
+
+                                <p><?php echo $wyjazd_zimowy->description ?></p>
+                                <!-- <td> <a href="<?php the_permalink(); ?>"> Zobacz</a></td> -->
+                                <a type="button" class="btn btn-outline-secondary" href="<?php the_permalink(); ?>">Zobacz więcej</a>
+                                <!-- <p><?php echo $wyjazd_zimowy->description_extra ?></p> -->
+                            </div>
+                        </div>
+                        <!-- KONIEC WPISU -->
+                    <?php endwhile; ?>
+                <?php endif; ?>
+                <?php {
+                    get_template_part('template-parts/content/error');
+                }
+                /**
+                 * Hook for anything after main content
+                 */
+                do_action('kadence_after_main_content');
+                ?>
         </main><!-- #main -->
         <?php
         get_sidebar();
